8dd5235094e7f7a2851e11624f36fe26c4eaa224
mohamed-hassan-747 <120794339+mohamed-hassan-747@users.noreply.github.com>
2024-10-12 14:56:25 +0300

    Add files via upload

---
+++ Req2/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    age = request.form.get('age')
+    email = request.form.get('email')
+    print(name)
+    return render_template('submit.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
